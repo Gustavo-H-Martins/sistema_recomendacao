3f355b81ca82a87e803cc127fd73ea02c76ecdd9
gustavolopesmartins <70769813+Gustavo-H-Martins@users.noreply.github.com>
2023-03-01 16:03:56 -0300

    .
    Signed-off-by: gustavolopesmartins <70769813+Gustavo-H-Martins@users.noreply.github.com>

--- sistemaRecomendacao.py
+++ sistemaRecomendacao.py
@@ -3,18 +3,16 @@ import pandas as pd
 import numpy as np
 
 # Importar o arquivo com os filmes e visualizar as primeiras linhas
-filmes = pd.read_csv('movies_metadata.csv', 
+filmes = pd.read_csv('movies_metadata.csv',
                      usecols=['id','original_title','original_language','vote_count'], 
-                     low_memory=False, 
-                     dtype={'id': int, 'vote_count': int},
-                     error_bad_lines=False)
+                     low_memory=False)
 
 # Importando o arquivo de avaliações e avaliando as primeiras linhas
 avaliacoes = pd.read_csv('ratings.csv', 
                          usecols=['userId','movieId','rating'], 
                          dtype={'userId':int,'movieId':int,'rating':float}, 
-                         nrows=1000000,
-                         error_bad_lines=False)
+                         nrows=10000000)
+
 
 # Renomeia as variaveis
 filmes.rename(columns = {'id':'ID_FILME','original_title':'TITULO','original_language':'LINGUAGEM','vote_count':'QT_AVALIACOES'}, inplace = True)
@@ -35,7 +33,7 @@ y = qt_avaliacoes[qt_avaliacoes].index
 avaliacoes = avaliacoes[avaliacoes['ID_USUARIO'].isin(y)]
 
 # Vamos usar os filmes que possuem somente uma quantidade de avaliações superior a 999 avaliações
-filmes = filmes.query('QT_AVALIACOES > 999')
+filmes = filmes[filmes['QT_AVALIACOES'] > 999]
 
 # Vamos agrupar e visualizar a quantidade de filmes pela linguagem
 filmes_linguagem = filmes['LINGUAGEM'].value_counts()
@@ -69,7 +67,7 @@ from scipy.sparse import csr_matrix
 
 
 # Vamos transformar o nosso dataset em uma matriz sparsa
-filmes_sparse = filmes_pivot.to_sparse()
+filmes_sparse = csr_matrix(filmes_pivot)
 
 # Vamos importar o algoritmo KNN do SciKit Learn
 from sklearn.neighbors import NearestNeighbors 
